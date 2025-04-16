430be7d95057f0834f2beb0bbfeaee5547d9dd12
Anshuman125 <121315837+Anshuman125@users.noreply.github.com>
2025-04-16 23:59:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,7 @@ import requests
 # Load model and tokenizer once for local processing (Streamlit's direct processing)
 @st.cache_resource
 def load_model():
-    model = AutoModelForSequenceClassification.from_pretrained(
-                "./legal-bert-cuad", use_safetensors=False
-            )
+    model = AutoModelForSequenceClassification.from_pretrained("./legal-bert-cuad")
     tokenizer = AutoTokenizer.from_pretrained("./legal-bert-cuad")
     return pipeline("text-classification", model=model, tokenizer=tokenizer, top_k=3)
 
